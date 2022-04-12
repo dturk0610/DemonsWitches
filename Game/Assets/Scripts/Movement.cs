23666e0155fa452ac1235141d3877127cd00716a
dakota.turk <dturk0610@gmail.com>
2022-04-11 20:30:22 -0400

    movement and scene

--- Game/Assets/Scripts/Movement.cs
+++ Game/Assets/Scripts/Movement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Transform cam;
+    public float moveSpeed = 10;
+    public float maxVel = 10;
+    Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( Input.GetKey(KeyCode.A) ){
+            rigidbody2D.AddForce( new Vector2( -moveSpeed, 0 ));
+        }else if ( Input.GetKey(KeyCode.D) ){
+            rigidbody2D.AddForce( new Vector2( moveSpeed, 0 ));
+        }
+        Vector2 currVel = rigidbody2D.velocity;
+        rigidbody2D.velocity = ( currVel.magnitude > maxVel ) ? currVel.normalized * maxVel : currVel;
+        cam.position = new Vector3( this.transform.position.x, cam.position.y, cam.position.z );
     }
 }
