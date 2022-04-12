d8aa572bcdd3f143c046be04fce158c3a0b80d6b
dakota.turk <dturk0610@gmail.com>
2022-04-12 16:06:59 -0400

    added jump

--- Game/Assets/Scripts/Movement.cs
+++ Game/Assets/Scripts/Movement.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Transform cam;
-    public float moveSpeed = 10;
+    public float moveSpeed = 10, jumpStrength  = 10;
     public float maxVel = 10;
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rb2D;
+    bool grounded = true;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = this.GetComponent<Rigidbody2D>();
+        rb2D = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( Input.GetKey(KeyCode.A) ){
-            rigidbody2D.AddForce( new Vector2( -moveSpeed, 0 ));
-        }else if ( Input.GetKey(KeyCode.D) ){
-            rigidbody2D.AddForce( new Vector2( moveSpeed, 0 ));
+        if ( Input.GetKey( KeyCode.A ) ){
+            rb2D.AddForce( new Vector2( -moveSpeed, 0 ));
+        }else if ( Input.GetKey( KeyCode.D ) ){
+            rb2D.AddForce( new Vector2( moveSpeed, 0 ));
         }
-        Vector2 currVel = rigidbody2D.velocity;
-        rigidbody2D.velocity = ( currVel.magnitude > maxVel ) ? currVel.normalized * maxVel : currVel;
+        Vector2 currVel = rb2D.velocity;
+        rb2D.velocity = ( currVel.magnitude > maxVel ) ? currVel.normalized * maxVel : currVel;
+
+        if ( Input.GetKeyDown( KeyCode.Space ) && grounded ){
+            Debug.Log("uhhh");
+            rb2D.AddForce( new Vector2( 0, jumpStrength ) );
+            grounded = false;
+        }
+
         cam.position = new Vector3( this.transform.position.x, cam.position.y, cam.position.z );
+    }
+
+    private void OnCollisionEnter2D( Collision2D other ) {
+        Debug.Log("grounded");
+        grounded = true;
     }
 }
