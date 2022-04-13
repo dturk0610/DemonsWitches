23e1cfc29f049d8a9f5c6a6e4ad8b07d4a2eaf7e
dakota.turk <dturk0610@gmail.com>
2022-04-12 22:13:08 -0400

    idle and walk animations

--- Game/Assets/Scripts/Movement.cs
+++ Game/Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour
     public Transform cam;
     public float moveSpeed = 10, jumpStrength  = 10;
     public float maxVel = 10;
+    public Animator anim;
     Rigidbody2D rb2D;
     bool grounded = true;
     // Start is called before the first frame update
@@ -20,14 +21,17 @@ public class Movement : MonoBehaviour
     {
         if ( Input.GetKey( KeyCode.A ) ){
             rb2D.AddForce( new Vector2( -moveSpeed, 0 ));
+            transform.localScale = new Vector3( -Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z );
         }else if ( Input.GetKey( KeyCode.D ) ){
             rb2D.AddForce( new Vector2( moveSpeed, 0 ));
+            transform.localScale = new Vector3( Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z );
         }
         Vector2 currVel = rb2D.velocity;
         rb2D.velocity = ( currVel.magnitude > maxVel ) ? currVel.normalized * maxVel : currVel;
 
+        anim.SetFloat( "Speed", Mathf.Abs( rb2D.velocity.magnitude ) );
+
         if ( Input.GetKeyDown( KeyCode.Space ) && grounded ){
-            Debug.Log("uhhh");
             rb2D.AddForce( new Vector2( 0, jumpStrength ) );
             grounded = false;
         }
@@ -36,7 +40,6 @@ public class Movement : MonoBehaviour
     }
 
     private void OnCollisionEnter2D( Collision2D other ) {
-        Debug.Log("grounded");
         grounded = true;
     }
 }
